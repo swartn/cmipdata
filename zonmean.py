203f2ed040858421cf7f433666a719768f702b7c
swartn <neil.cameron.swart@gmail.com>
2014-03-07 14:33:51 -0800

    minor update, added zonmean.py

---
+++ zonmean.py
@@ -0,0 +1,19 @@
+import os
+import glob
+
+def zonmean(filenames, delete=False):
+    """
+    Zonal mean each file in filenames using CDO, and do a smart naming of the output and remove the mess (input files) if delete=True
+    """ 
+
+    for cfile in filenames:
+        print 'zonal mean of: ', cfile
+        infile = cfile
+        outfile = ' xm_' + cfile
+        catstring = 'cdo zonmean ' + infile + outfile 			
+        os.system( catstring )
+
+        if delete == True
+            delstr = 'rm -f' + cfile
+	    os.system( delstr )
+ 
