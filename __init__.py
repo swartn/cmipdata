3c2edd2c9c2778f60f168a90d07aae5c1ea05d47
swartn <neil.cameron.swart@gmail.com>
2014-03-07 17:01:57 -0800

    introduced loaddata

--- __init__.py
+++ __init__.py
@@ -1,8 +1,9 @@
 import os
 import glob
 
-__all__ = ["listfiles" , "join_exp_slice", "zonmean"]
+__all__ = ["listfiles" , "join_exp_slice", "zonmean", "loaddata"]
 
 from listfiles import *
 from join_exp_slice import *
 from zonmean import *
+from loaddata import *
