221ab2276ab11e3ad074b72300600815bdcd2a3d
swartn <neil.cameron.swart@gmail.com>
2014-03-13 15:59:57 -0700

    updated init to not fail for missing cdo

--- __init__.py
+++ __init__.py
@@ -6,4 +6,7 @@ __all__ = ["listfiles" , "join_exp_slice", "zonmean", "loaddata"]
 from listfiles import *
 from join_exp_slice import *
 from zonmean import *
-from loaddata import *
+try:
+    from loaddata import *
+except ImportError:
+    pass
