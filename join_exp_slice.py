c180ecc250202bf979a9ac36041c7ab879a9df5b
Neil Swart <acrnncs@lxsrv2.cccma.ec.gc.ca>
2014-04-09 15:07:51 -0700

    updated remap to work and added a match_experiments function to join_exp_slice

--- join_exp_slice.py
+++ join_exp_slice.py
@@ -38,3 +38,34 @@ def join_exp_slice( filenames , modelnames ):
 		  for cfile in modfiles:
 		      delstr = 'rm ' + cfile
 		      os.system( delstr ) 
+
+
+def match_exp( filenames , modelnames, rcpname='rcp45' ):
+    """
+    Do a concatenation of multiple time-slice CMIP type nc files using CDO, and do a smart naming of the output (give the correct year-range in the output file name) and remove the mess (input files). Should be made more general.
+    """
+
+    for mod in modelnames:
+       print "cmipdata.match_exp: Model ", mod 
+       modfilesall = [ filename for filename in filenames if filename.split( '_' )[2] == mod ]
+
+       modfiles_historical = [ filename for filename in modfilesall if filename.split( '_' )[3] == 'historical' ]
+       ensmembers_hist = [ modfile.split('_')[4] for modfile in modfiles_historical ]
+       uniq_ensmems_hist = sorted( set( ensmembers_hist ) )
+
+       modfiles_rcp = [ filename for filename in modfilesall if filename.split( '_' )[3] == rcpname ]
+       ensmembers_rcp = [ modfile.split('_')[4] for modfile in modfiles_rcp ]
+       uniq_ensmems_rcp = sorted( set( ensmembers_rcp ) )
+
+       for ensmem in uniq_ensmems_hist:
+               modfiles = [ modfilename for modfilename in modfilesall if
+                            modfilename.split( '_' )[4] == ensmem ]
+
+               if any( ensmem in s for s in uniq_ensmems_rcp):
+                   print "Historical - RCP match for: ", ensmem
+               else:
+                   print " NO match for: ", ensmem, "...deleting"
+                   for cfile in modfiles:
+                       delstr = 'rm ' + cfile
+                       os.system(delstr)
+       print 
