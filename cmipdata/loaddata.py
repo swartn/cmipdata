53310d000d51f5e41463f303878d6e20e94242d3
swartn <Neil.Swart@ec.gc.ca>
2014-07-16 14:01:21 -0700

    Adds 'zonmean' option to loaddata and a unit test.

--- cmipdata/loaddata.py
+++ cmipdata/loaddata.py
@@ -1,24 +1,51 @@
 import cdo as cdo; cdo = cdo.Cdo() # recommended import
 import os
 from numpy import squeeze
+from netCDF4 import Dataset
+
 
 os.system( 'rm -rf /tmp/cdo*') # clean out tmp to make space for CDO processing.
 
-def loadvar( ifile , varname, remap='', start_date='', end_date='', timmean=False):
+def loadvar( ifile , varname, remap='', start_date='', end_date='', timmean=False, zonmean=False):
         """  
             Load a CMIP5 netcdf variable "varname" from "ifile" and optionally 1) distance
             weighted remap to a given grid (e.g. 'r360x180), 2) select a date range between 
-            start_date and end_date (format: 'YYYY-MM-DD') and 3) time-mean over the whole 
-            record, or between the selected dates. Requires netCDF4, CDO and CDO python bindings.
-            Returns a masked array, var.
-        """
-
+            start_date and end_date (format: 'YYYY-MM-DD'), 3) time-mean over the whole 
+            record, or between the selected dates and 4) zonal mean. Requires netCDF4, CDO 
+            and CDO python bindings. Returns a masked array, var.
+            
+            If zonmean=True and remap=True, the zonal mean is done first, so the remap
+            will in that case only specify the latitude-grid, with 1-point in x, e.g.: 
+            remap=r180x1.
+          """
+          
+        # Open the variable using NetCDF4 to get scale and offset attributes.  
+        nc = Dataset( ifile , 'r' )
+        ncvar = nc.variables[ varname ]
+        
         date_range = start_date + ',' + end_date
 
-        if ( timmean == True ) and ( start_date ) and ( remap ) :
-            in_str = "-timmean -seldate," + date_range + " " + ifile
+        # parse through all options, and load the data using CDO.
+        if ( timmean == True ) and ( start_date ) and ( remap ) and (zonmean == True) :
+            in_str = "-zonmean -timmean -seldate," + date_range + "  -selvar," + varname + " " + ifile
             var = cdo.remapdis( remap , input = in_str, returnMaArray=varname )
 
+        elif ( timmean == True ) and ( start_date ) and ( remap ) :
+            in_str = "-timmean -seldate," + date_range + " " + ifile
+            var = cdo.remapdis( remap , input = in_str, returnMaArray=varname )
+  
+        elif ( timmean == True ) and ( start_date ) and ( zonmean ) :
+            in_str = "-timmean -seldate," + date_range + " -selvar," + varname + " " + ifile
+            var = cdo.zonmean( input = in_str, returnMaArray=varname )           
+    
+        elif ( timmean == True ) and ( zonmean ) and ( remap ) :
+            in_str = "-zonmean -timmean -selvar," + varname + " " + ifile
+            var = cdo.remapdis( remap , input = in_str, returnMaArray=varname )     
+  
+        elif ( zonmean ) and ( start_date ) and ( remap ) :
+            in_str = "-zonmean -seldate," + date_range + " -selvar," + varname + " " + ifile
+            var = cdo.remapdis( remap , input = in_str, returnMaArray=varname )   
+  
         elif ( timmean == True ) and ( remap ) :
             in_str = "-timmean" + " " + ifile
             var = cdo.remapdis( remap , input = in_str, returnMaArray=varname )
@@ -30,7 +57,19 @@ def loadvar( ifile , varname, remap='', start_date='', end_date='', timmean=Fals
         elif  ( timmean == True ) and ( start_date ):
             var = cdo.timmean( input = cdo.seldate( date_range, input=ifile ), 
                                returnMaArray=varname )
-
+                               
+        elif  ( timmean == True ) and ( zonmean == True ):
+	    in_str = "-timmean -selvar," + varname + " " + ifile
+            var = cdo.zonmean( input=in_str, returnMaArray=varname )       
+            
+        elif  ( start_date ) and ( zonmean == True ):
+	    in_str = "-seldate," + date_range +  " -selvar," + varname + " " + ifile
+            var = cdo.zonmean( input=in_str, returnMaArray=varname )    
+            
+        elif  ( remap ) and  ( zonmean == True ):
+            in_str = "-zonmean -selvar," + varname + " " + ifile
+            var = cdo.remapdis( remap , input = in_str, returnMaArray=varname )            
+            
         elif ( remap ) :
             var = cdo.remapdis( remap , input = ifile, returnMaArray=varname )
 
@@ -40,12 +79,25 @@ def loadvar( ifile , varname, remap='', start_date='', end_date='', timmean=Fals
         elif ( start_date ):
             var = cdo.seldate( date_range, input=ifile, returnMaArray=varname )
 
+        elif ( zonmean == True ):
+            in_str =  "-selvar," + varname + " " + ifile
+            var = cdo.zonmean( input=in_str, returnMaArray=varname )
+
         else :
-            from netCDF4 import Dataset
-            nc = Dataset( ifile , 'r' )
-            var = nc.variables[ varname ]
+            var = ncvar[:]      
             
-        return var
-        #return squeeze( var )
+        # Apply any scaling and offsetting needed:
+        try:
+	    var_offset = ncvar.add_offset
+        except:
+	    var_offset = 0
+        try:
+	    var_scale = ncvar.scale_factor
+        except:
+	    var_scale = 1	
+            
+        var = var*var_scale + var_offset    
+        #return var
+        return squeeze( var )
          
 
---
+++ cmipdata/test/test_loadvar.py
@@ -0,0 +1,114 @@
+import cmipdata as cd
+from netCDF4 import Dataset
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+# set font size
+plt.close('all')
+plt.ion()
+import os
+os.system( 'rm -rf /tmp/cdo*') # clean out tmp to make space for CDO processing.
+
+# Define the location of the data
+rean_data_path = '/HOME/ncs/data/reanalyses/'
+infile = rean_data_path +  'R1_slp.mon.mean.nc'
+
+# get out the latitudes
+nc = Dataset( infile , 'r' )
+slp = nc.variables[ 'slp' ]
+ind = [ j for j, dim in enumerate(slp.dimensions) if dim.lower().startswith('lat') ]
+lat = cd.loadvar(infile, slp.dimensions[ind[0]])
+
+# test timnean, startdate, zonmean and remap
+print "timnean, startdate, remap and zonmean"
+slpclim = cd.loadvar( infile, varname='slp', remap='r180x1', start_date='1979-01-01', end_date='2009-12-31', timmean=True, zonmean=True)
+print slpclim.shape
+
+# test timnean, startdate and remap
+print "timnean, startdate and remap"
+slpclim = cd.loadvar( infile, 'slp', remap='r360x180', start_date='1979-01-01', end_date='2009-12-31', timmean=True)
+print slpclim.shape
+
+# test timnean, startdate and zonmean
+print "timnean, startdate and zonmean"
+slpclim = cd.loadvar( infile, 'slp', start_date='1979-01-01', end_date='2009-12-31', timmean=True, zonmean=True)
+print slpclim.shape
+
+# test timnean, zonmean and remap
+print "timnean, zonmean and remap"
+slpclim = cd.loadvar( infile, 'slp', remap='r180x1', timmean=True, zonmean=True)
+print slpclim.shape
+
+# test startdate, zonmean and remap
+print "startdate, remap and zonmean"
+slpclim = cd.loadvar( infile, varname='slp', remap='r180x1', start_date='1979-01-01', end_date='2009-12-31', zonmean=True)
+print slpclim.shape
+
+# test timnean and remap
+print "timnean remap"
+slpclim = cd.loadvar( infile, varname='slp', remap='r360x180', timmean=True)
+print slpclim.shape
+
+# test startdate and remap
+print "startdate and remap"
+slpclim = cd.loadvar( infile, 'slp', remap='r360x180', start_date='1979-01-01', end_date='2009-12-31')
+print slpclim.shape
+
+# test timnean, startdate, 
+print "timnean and startdate"
+slpclim = cd.loadvar( infile, varname='slp', start_date='1979-01-01', end_date='2009-12-31', timmean=True)
+print slpclim.shape
+
+# test timnean and zonmean
+print "timneanand zonmean"
+slpclim = cd.loadvar( infile, varname='slp', timmean=True, zonmean=True)
+print slpclim.shape
+
+# startdate and zonmean
+print "startdate and zonmean"
+slpclim = cd.loadvar( infile, varname='slp', start_date='1979-01-01', end_date='2009-12-31', zonmean=True)
+print slpclim.shape
+
+# test zonmean and remap
+print "remap and zonmean"
+slpclim = cd.loadvar( infile, varname='slp', remap='r180x1', zonmean=True)
+print slpclim.shape
+
+# test  remap
+print "remap"
+slpclim = cd.loadvar( infile, varname='slp', remap='r360x180')
+print slpclim.shape
+
+# load timmean
+print "timmean"
+slpclim = cd.loadvar( infile, varname='slp', timmean=True )
+print slpclim.shape
+
+# startdate 
+print "startdate"
+slpclim = cd.loadvar( infile, varname='slp', start_date='1979-01-01', end_date='2009-12-31')
+print slpclim.shape
+
+# load zonmean
+print "zonmean"
+slpclim = cd.loadvar( infile, varname='slp', zonmean=True )
+print slpclim.shape
+
+# a vanilla load
+print "load"
+slpclim = cd.loadvar( infile, varname='slp' )
+print slpclim.shape
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#plt.plot( lat, slpclim)
